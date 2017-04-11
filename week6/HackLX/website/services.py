2d3b37101ff119b6463eae57333c06395fc2aad0
Premitium <simeonparvanov@ymail.com>
2017-04-11 13:17:00 +0300

    Removed test code from services

--- week6/HackLX/website/services.py
+++ week6/HackLX/website/services.py
@@ -1,11 +1,5 @@
 from .models import Offer
 
-def delete_offer(request, pk):
-    offer = get_object_or_404(Offer, pk=int(pk))
-    offer.delete()
-    return redirect(reverse('website:index'))
-
-
 def get_categoty_names(Category):
     result_title_names = []
     query_set_tuples = Category.objects.values_list('name')
@@ -13,5 +7,4 @@ def get_categoty_names(Category):
     for abs in query_set_tuples:
         result_title_names.insert(len(result_title_names), ''.join(abs))
 
-    # import ipdb; ipdb.set_trace()
     return result_title_names
